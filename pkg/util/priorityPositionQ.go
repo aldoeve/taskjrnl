996c393ae9a63c30f4b8abc36c6f475a67d04d9e
aldoeve <aldoeve@example.com>
2026-04-04 22:07:50 -0500

    Added: Small and simple priority queue for position table

--- pkg/util/priorityPositionQ.go
+++ pkg/util/priorityPositionQ.go
@@ -1,14 +1,55 @@
 package util
 
+import (
+	"container/heap"
+	schema "taskjrnl/internal/schema"
+)
+
 type PositonItem struct {
-	task_id  int
-	priority int
-	index    int
+	taskId     int
+	importance int
+	index      int
 }
 type PositonPriorityQueue []*PositonItem
 
 func (pq PositonPriorityQueue) Len() int { return len(pq) }
 
 func (pq PositonPriorityQueue) Less(i, j int) bool {
-	return pq[i].priority > pq[j].priority
+	return pq[i].importance > pq[j].importance
+}
+
+func (pq PositonPriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = j
+	pq[j].index = i
+}
+
+// Pushes a task id from a given task onto the priority queue.
+func (pq *PositonPriorityQueue) Push(x any) {
+	task := x.(*schema.Tasks)
+
+	item := PositonItem{
+		taskId:     task.Id,
+		importance: CalculateImportance(task),
+		index:      len(*pq),
+	}
+
+	*pq = append(*pq, &item)
+}
+
+// Returns taskId with the highest priority.
+func (pq *PositonPriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	positonItem := old[n-1]
+	old[n-1] = nil
+	positonItem.index = -1
+	*pq = old[0 : n-1]
+	return positonItem.taskId
+}
+
+func (pq *PositonPriorityQueue) update(positionItem *PositonItem, taskId int, importance int) {
+	positionItem.taskId = taskId
+	positionItem.importance = importance
+	heap.Fix(pq, positionItem.index)
 }
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,9 +1,35 @@
 // Package util holds commonly used utility functions.
 package util
 
-import "flag"
+import (
+	"flag"
+	consts "taskjrnl/internal/consts"
+	schema "taskjrnl/internal/schema"
+	"time"
+)
 
 // This function assumes the first token is a keyword and discards it.
 func ArgsAfterKeyword() []string {
 	return flag.Args()[1:]
+}
+
+var PriorityValue = map[string]int{
+	consts.LowPriority:  0,
+	consts.MidPriority:  1000,
+	consts.HighPriority: 2000,
+}
+
+func CalculateImportance(task *schema.Tasks) int {
+	priority := PriorityValue[*task.Priority]
+
+	var daysSinceCreation int
+	layout := "2009-09-25 15:09:01" // SQLite TEXT format.
+	storedTime, err := time.Parse(layout, task.DateCreated)
+	if err == nil {
+		daysSinceCreation = int(time.Since(storedTime).Hours() / 24)
+	}
+
+	finalCalculation := priority + daysSinceCreation + task.ImportanceVariance
+
+	return finalCalculation
 }
