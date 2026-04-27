7bacc8f7097025f88f16ce7258f8760a3d0902a8
aldoeve <aldoeve@example.com>
2026-04-26 21:07:57 -0500

    Added: New tests for the priority queue

--- pkg/util/positionPQ.go
+++ pkg/util/positionPQ.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"container/heap"
 	schema "taskjrnl/internal/schema"
 )
 
@@ -10,22 +9,22 @@ type PositonItem struct {
 	importance int
 	index      int
 }
-type PositonPriorityQueue []*PositonItem
+type PositionPriorityQueue []*PositonItem
 
-func (pq PositonPriorityQueue) Len() int { return len(pq) }
+func (pq PositionPriorityQueue) Len() int { return len(pq) }
 
-func (pq PositonPriorityQueue) Less(i, j int) bool {
+func (pq PositionPriorityQueue) Less(i, j int) bool {
 	return pq[i].importance > pq[j].importance
 }
 
-func (pq PositonPriorityQueue) Swap(i, j int) {
+func (pq PositionPriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = j
-	pq[j].index = i
+	pq[i].index = i
+	pq[j].index = j
 }
 
 // Pushes a task id from a given task onto the priority queue.
-func (pq *PositonPriorityQueue) Push(x any) {
+func (pq *PositionPriorityQueue) Push(x any) {
 	task := x.(*schema.Tasks)
 
 	item := PositonItem{
@@ -38,7 +37,7 @@ func (pq *PositonPriorityQueue) Push(x any) {
 }
 
 // Returns taskId with the highest priority.
-func (pq *PositonPriorityQueue) Pop() any {
+func (pq *PositionPriorityQueue) Pop() any {
 	old := *pq
 	n := len(old)
 	positonItem := old[n-1]
@@ -48,8 +47,8 @@ func (pq *PositonPriorityQueue) Pop() any {
 	return positonItem.taskId
 }
 
-func (pq *PositonPriorityQueue) update(positionItem *PositonItem, taskId int, importance int) {
+/* func (pq *PositionPriorityQueue) update(positionItem *PositonItem, taskId int, importance int) {
 	positionItem.taskId = taskId
 	positionItem.importance = importance
 	heap.Fix(pq, positionItem.index)
-}
+} */
