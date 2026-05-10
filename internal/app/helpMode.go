0507543adc9a9774c56b911ac149cd50cecc142a
aldoeve <aldoeve@example.com>
2026-05-10 11:21:31 -0500

    Changes: separated drawing power in helpmode.go to imporve readability

--- internal/app/helpMode.go
+++ internal/app/helpMode.go
@@ -13,7 +13,42 @@ type BaseArgStruct struct {
 	usage string
 }
 type Command = BaseArgStruct
-type Flag = BaseArgStruct
+type Options = BaseArgStruct
+
+// Returns a string of formated arguements and their description.
+func buildArgsNDesc(finalOutput string, args []BaseArgStruct) string {
+	for _, argObj := range args {
+		argName := consts.HelpCommandsNFlagsText.Render("\t" + argObj.name)
+		argUsageDesc := consts.HelpUsageText.Render(argObj.usage)
+		argRow := lipgloss.JoinHorizontal(lipgloss.Left, argName, argUsageDesc)
+
+		finalOutput = lipgloss.JoinVertical(lipgloss.Left, finalOutput, argRow)
+	}
+
+	return finalOutput
+}
+
+// Returns the help screen options section.
+func buildOptionsSection() string {
+	optionsSection := consts.HelpTitleStyle.Render("Options:")
+	flags := []Options{
+		{"-h, --help", "Show help"},
+	}
+
+	return buildArgsNDesc(optionsSection, flags)
+}
+
+// Returns the help screen commands section.
+func buildCommnadsSection() string {
+	commandsSection := consts.HelpTitleStyle.Render("Commands:")
+	commands := []Command{
+		{appmodes.Help, "Show help"},
+		{appmodes.Add, "Adds task. <taskName> [priority:L|M|H] [tag:\"string\"]"},
+		{appmodes.List, "Lists all tasks"},
+	}
+
+	return buildArgsNDesc(commandsSection, commands)
+}
 
 // Draws help output to stdout.
 func drawHelp() {
@@ -25,33 +60,9 @@ func drawHelp() {
 	usageText := consts.HelpUsageText.Render("\n\ttaskjrnl | task [options] <command>")
 	finalUsage := lipgloss.JoinVertical(lipgloss.Left, usageTabTitle, usageText)
 
-	commandsSection := consts.HelpTitleStyle.Render("Commands:")
-	commands := []Command{
-		{appmodes.Help, "Show help"},
-		{appmodes.Add, "Adds task. <taskName> [priority:L|M|H] [tag:\"string\"]"},
-		{appmodes.List, "Lists all tasks"},
-	}
+	commandsSection := buildCommnadsSection()
 
-	for _, commandObj := range commands {
-		command := consts.HelpCommandsNFlagsText.Render("\t" + commandObj.name)
-		commandUsageDesc := consts.HelpUsageText.Render(commandObj.usage)
-		commandRow := lipgloss.JoinHorizontal(lipgloss.Left, command, commandUsageDesc)
-
-		commandsSection = lipgloss.JoinVertical(lipgloss.Left, commandsSection, commandRow)
-	}
-
-	optionsSection := consts.HelpTitleStyle.Render("Options:")
-	flags := []Flag{
-		{"-h, --help", "Show help"},
-	}
-
-	for _, flagObj := range flags {
-		flagName := consts.HelpCommandsNFlagsText.Render("\t" + flagObj.name)
-		flagUsageDesc := consts.HelpUsageText.Render(flagObj.usage)
-		flagRow := lipgloss.JoinHorizontal(lipgloss.Left, flagName, flagUsageDesc)
-
-		optionsSection = lipgloss.JoinVertical(lipgloss.Left, optionsSection, flagRow)
-	}
+	optionsSection := buildOptionsSection()
 
 	final := lipgloss.JoinVertical(
 		lipgloss.Left,
